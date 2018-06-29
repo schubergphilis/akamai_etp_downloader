82f63f6dd98d6b28cdd81be890e75c06d46fb9c0
Theodoor Scholte <tscholte@sbplt90e3hv2j.sbp.lan>
2018-06-30 00:55:43 +0200

    fix wrapper script

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [ ! -x ~/.local/bin/pipenv ] && pip install --user pipenv;
-'~/.local/bin/pipenv install'
-"~/.local/bin/pipenv run python $1/etp_downloader.py -d $2 -l $3 -s $4 -D"
+~/.local/bin/pipenv install
+~/.local/bin/pipenv run python $1/etp_downloader.py -d $2 -l $3 -s $4 -D
