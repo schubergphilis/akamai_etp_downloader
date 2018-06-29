a85e6a12dcc6aa8faf5fbbae4354df8d8abb683b
Theodoor Scholte <tscholte@sbplt90e3hv2j.sbp.lan>
2018-06-29 23:36:10 +0200

    display python version

--- wrapper.sh
+++ wrapper.sh
@@ -2,8 +2,8 @@
 echo 'Python version: '
 python --version
 [ ! -x ~/.local/bin/pipenv ] && pip install --user pipenv;
-~/.local/bin/pipenv install
+~/.local/bin/pipenv install --python 2.7.13
 cd $1
-~/.local/bin/pipenv run python $2/etp_downloader.py -d $3 -l $4 -s $5 -D
+~/.local/bin/pipenv run python $2/etp_downloader.py -d $3 -l $4 -s $5 -D --python 2.7.13
 
 
