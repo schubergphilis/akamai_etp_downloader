d7fe7a61af3236ea5f80e196e7f1bf49c8fd3423
Theodoor Scholte <tscholte@sbplt90e3hv2j.sbp.lan>
2018-06-30 00:02:31 +0200

    fix wrapper script

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 [ ! -x ~/.local/bin/pipenv ] && pip install --user pipenv;
-~/.local/bin/pipenv install --python 2.7.13
-export LD_LIBRARY_PATH=/opt/rh/python27/root/usr/lib64
+scl enable python27 '~/.local/bin/pipenv install'
 cd $1
-~/.local/bin/pipenv run python $2/etp_downloader.py -d $3 -l $4 -s $5 -D --python 2.7.13
+scl enable python27 '~/.local/bin/pipenv run python $2/etp_downloader.py -d $3 -l $4 -s $5 -D'
 
 
