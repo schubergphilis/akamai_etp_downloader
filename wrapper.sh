5e0b3d3d9eb03dcce2042d171d76b4d3ab7a68ac
Theodoor Scholte <tscholte@sbplt90e3hv2j.sbp.lan>
2018-07-04 12:07:07 +0200

    redirect pip output to /dev/null

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[ ! -x ~/.local/bin/pipenv ] && pip install --user pipenv;
-~/.local/bin/pipenv install
+[ ! -x ~/.local/bin/pipenv ] && pip install --user pipenv > /dev/null 2>&1;;
+~/.local/bin/pipenv install > /dev/null 2>&1;
 ~/.local/bin/pipenv run python $1/etp_downloader.py -d $2 -l $3 -s $4 -D
